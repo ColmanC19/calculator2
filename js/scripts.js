9bffa7a47b504e11d55ace4a93efb8095330c31e
Student <example@example.com>
2019-09-18 18:32:58 -0700

    Updated calculator with branching

--- js/scripts.js
+++ js/scripts.js
@@ -19,36 +19,21 @@ var divide = function(number1, number2) {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-  // ADDITION
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#addOutput").text(result);
-  });
-    // SUBTRACTION
-    $("form#subtract").submit(function(event) {
-      event.preventDefault();
-      var number1 = parseInt($("#minus1").val());
-      var number2 = parseInt($("#minus2").val());
-      var result = subtract(number1, number2);
-      $("#subtractOutput").text(result);
-  });
-  // MULTIPLICATION
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#times1").val());
-    var number2 = parseInt($("#times2").val());
-    var result = multiply(number1, number2);
-    $("#multiplyOutput").text(result);
-  });
-  // DIVISION
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var result = divide(number1, number2);
-    $("#divideOutput").text(result);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
   });
 });
